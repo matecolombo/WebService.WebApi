73dc1aaadc71e89766b7c665cfd952180a8abb97
Mateo Colombo <mateo.colombo95@gmail.com>
2020-05-08 12:10:13 -0300

    Se crean controladores de camion, WebService: api, dominio, cliente web, datos

---
+++ Controllers/CamionController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebService.Datos.Modelos;
+
+namespace WebService.WebApi.Controllers
+{
+    public class CamionController : ApiController
+    {
+        BalanzaConection BD = new BalanzaConection();
+
+        //Permite consultar la info de todas las patentes
+
+        [HttpGet]
+        public IEnumerable<Camion> Get()
+        {
+            var listado = BD.Camion.ToList();
+            return listado;
+        }
+
+        [HttpGet]
+        public Camion Get(string Patente)
+        {
+            var camion = BD.Camion.FirstOrDefault(x => x.PATENTE == Patente);
+            return camion;
+        }
+
+        [HttpPost]
+        public bool Post(Camion camion)
+        {
+            BD.Camion.Add(camion);
+            return BD.SaveChanges() > 0;
+        }
+
+        [HttpPut]
+        public bool Put(Camion camion)
+        {
+            var camionActualizar = BD.Camion.FirstOrDefault(x => x.PATENTE == camion.PATENTE);
+            camionActualizar.EJES1 = camion.EJES1;
+            camionActualizar.EJES2 = camion.EJES2;
+            camionActualizar.ADVERTENCIAS = camion.ADVERTENCIAS;
+
+            return BD.SaveChanges() > 0;
+        }
+
+        [HttpDelete]
+        public bool Delete(string patente)
+        {
+            var camionEliminar = BD.Camion.FirstOrDefault(x => x.PATENTE == patente);
+            BD.Camion.Remove(camionEliminar);
+            return BD.SaveChanges() > 0;
+
+        }
+    }
+}
